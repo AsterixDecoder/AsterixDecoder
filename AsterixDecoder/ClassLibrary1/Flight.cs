5f6d3e501a497bcc511733a091faf239d1f501f1
MARC XAPELLI JAIME <marc.xapelli@estudiantat.upc.edu>
2021-11-22 23:11:33 +0100

    Cambios

--- AsterixDecoder/ClassLibrary1/Flight.cs
+++ AsterixDecoder/ClassLibrary1/Flight.cs
@@ -4,7 +4,42 @@ using System.Text;
 
 namespace ClassLibrary
 {
-    class Flight
+    public class Flight
     {
+        string identification;
+        List<Coordinates> coordinatesCAT21;
+        List<TimeSpan> timestampsCAT21;
+        List<Coordinates> coordinatesCAT10;
+        List<TimeSpan> timestampsCAT10;
+        public Flight(string id)
+        {
+            this.identification = id;
+            coordinatesCAT21 = new List<Coordinates>();
+            timestampsCAT21 = new List<TimeSpan>();
+            coordinatesCAT10 = new List<Coordinates>();
+            timestampsCAT10 = new List<TimeSpan>();
+        }
+
+    }
+    public class Coordinates
+    {
+        double latitude;
+        double longitude;
+
+        public Coordinates(double latitude, double longitude)
+        {
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        public double GetLatitude()
+        {
+            return this.latitude;
+        }
+
+        public double GetLongitude()
+        {
+            return this.longitude;
+        }
     }
 }
